e1789cdd19cc700929ba1f9f2e8fce8191091143
Oceanintheocean <oceanhong0326@163.com>
2021-07-09 10:20:54 +0800

    ocean1

---
+++ Ocean Hong Homework.py
@@ -0,0 +1,25 @@
+ # This function should return the maximum value in the array a of n elements. 
+ #   We assume n > 0.
+ #
+ #   The function has a bug.  You need to:
+ #   (1) Add a test case to main that exposes the bug.
+ #   (2) Fix the bug.
+ #   (3) Submit your changes on GitHub as a pull request
+def max(n, a):
+    assert n > 0
+    max = a[n-1]
+    for i in range(n-1, 0, -1):
+        if a[i] > max:
+            max = a[i]
+
+    return max
+
+
+a = [10,8]
+max_a = max(2, a)
+print("max(2, a) = " + str(max_a))
+
+
+b = [90,7, 3, 5, 3, 7, 9, 4]
+max_b = max(7, b)
+print("max(7, b) = " + str(max_b))
